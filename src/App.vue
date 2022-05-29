a1af550dd17e757d4b77bf31adc20df5463d4a7d
Your Name <takuyarossi77@gmail.com>
2022-05-29 13:04:53 +0900

    learn how directive works

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,19 @@
 export default {
     data() {
         return {
-            number:0
+            number:0,
+            items:[1,2,3],
+            color:""
         }
     },
     methods: {
         countup(){
             this.number = this.number + 1
+        }
+    },
+    computed: {
+        custom_color(){
+            return this.color.toUpperCase();
         }
     }
 }
@@ -17,5 +24,24 @@ export default {
 <div>
     <h1>{{number}}</h1>
     <button @click="countup">countup</button>
+    <p v-if="number">0以上</p>
+    <p v-else>0</p>
+    <ul>
+        <li v-for="(item,index) in items" :key="index">{{item}}</li>
+    </ul>
+    <p :class="color">カラー</p>
+    <input type="text" v-model="color">
+    <p>{{custom_color}}</p>
 </div>
 </template>
+<style>
+.red {
+    color: red;
+}
+.green {
+    color: green;
+}
+.blue {
+    color: blue;
+}
+</style>
