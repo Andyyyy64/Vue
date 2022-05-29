1b37356faaa351f78f3231843ce3d8bcf285b709
Your Name <takuyarossi77@gmail.com>
2022-05-29 11:58:24 +0900

    created by my self feels good

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    data() {
+        return {
+            number:0
+        }
+    },
+    methods: {
+        countup(){
+            this.number = this.number + 1
+        }
+    }
+}
+</script>
+
+<template>
+<div>
+    <h1>{{number}}</h1>
+    <button @click="countup">countup</button>
+</div>
+</template>
