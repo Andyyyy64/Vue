7fd4c1931626d1c4e0fcd7e18d24b999bd9e5b3a
Your Name <takuyarossi77@gmail.com>
2022-05-30 15:45:54 +0900

    creating ToDolist with qiita

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,44 @@
 export default {
     data(){
         return {
-            items:[1,2,3],
-            show: true
+            list:[],
+            addtext:"",
+            keyword:""
         }
     },
+    methods: {
+        addtodo(){
+            if(this.addtext !== ""){
+                this.list.push({
+                    text:this.addtext,
+                    isChecked:false,
+                });
+            }
+            this.addtext = "";
+        },
+        deleteBtn(){
+            this.list = this.list.filter(function(todo){
+                return !todo.isChecked;
+            });
+        }
+    }
 }
 </script>
 
 <template>
-<div>
-    <button @click="show = !show">togglelist</button>
-    <ul v-if="show">
-        <li v-for="(item,index) in items" :key="index">{{item}}</li>
-    </ul>
-    <p v-else>list is not empty but hidden</p>
+<div class="container">
+    <input type="text" v-model="addtext" placeholder="ToDoを入力して">
+    <button @click="addtodo">追加</button>
+    <button @click="deleteBtn">削除</button>
+    <div v-if="list.length">
+        <ul v-for="(todo,index) in list" :key="index">
+            <li>
+                <span :class="{done:todo.isChecked}">
+                <input type="checkbox" v-model="todo.isChecked">{{todo.text}}
+                </span>
+            </li>
+        </ul>
+    </div>
+    <div v-else>現在タスクはありません</div>
 </div>
 </template>
