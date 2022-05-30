02b5f195fac4883f34680a9aeadf69d3ff2c0720
Your Name <takuyarossi77@gmail.com>
2022-05-30 17:10:11 +0900

    todolist

--- src/App.vue
+++ src/App.vue
@@ -25,10 +25,10 @@ export default {
     },
     computed: {
         remaining(){
-            conut = 0;
-            todos = this.list;
-            length = todos.length;
-            for(i = 0; i<length; i++){
+            var count = 0;
+            var todos = this.list;
+            var length = todos.length;
+            for(var i = 0; i<length; i++){
                 if(!todos[i].isChecked){
                     count++;
                 }
@@ -42,6 +42,7 @@ export default {
 
 <template>
 <div class="container">
+    <div>残タスク:{{remaining}}/{{list.length}}</div>
     <input type="text" v-model="addtext" placeholder="ToDoを入力して">
     <button @click="addtodo">追加</button>
     <button @click="deleteBtn">削除</button>
