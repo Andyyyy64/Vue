6e08efb9cec771aed545f94879022c6cf8202054
Your Name <takuyarossi77@gmail.com>
2022-05-31 19:23:50 +0900

    add some css

--- src/App.vue
+++ src/App.vue
@@ -42,9 +42,9 @@ export default{
 <div class="container">
     <div>残りタスク:{{remaining}}/{{list.length}}
     </div>
-    <input type="text" v-model="addText" placeholder="ToDoを入力して">
-    <button @click="addtodo">追加</button>
-    <button @click="deleteBtn">削除</button>
+    <input class="mr-10 mb-10 mt-10" type="text" v-model="addText" placeholder="ToDoを入力して">
+    <button class="add mr-10"  @click="addtodo">追加</button>
+    <button class="dele mr-10"  @click="deleteBtn">削除</button>
     <div v-if="list.length">
         <ul v-for="(todo,index) in list" :key="index">
             <li>
@@ -56,4 +56,28 @@ export default{
     </div>
     <div v-else>現在のタスクはありません</div>
 </div>
-</template>         
+</template>
+<style>
+ul{
+    list-style: none;
+}
+.container{
+    margin: 0 auto;
+    text-align: center;
+}
+.add{
+    color:blue;
+}
+.dele{
+    color: red;
+}
+.mr-10{
+    margin-right: 10px;
+}
+.mb-10{
+    margin-bottom: 10px;
+}
+.mt-10{
+    margin-top: 10px;
+}
+</style>
